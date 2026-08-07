463e7dae8dd3a052cb20a8e56c88057cce2cf41a
Brok3nPix3l <rjbabala@yahoo.com>
2026-08-07 16:30:27 -0400

    added tailwind import

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/watfin-do-you-know-hearthstone/',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
