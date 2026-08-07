bb08a3f722d1fdfa09af45561cd237c6872f4e9a
Brok3nPix3l <rjbabala@yahoo.com>
2026-08-07 16:20:20 -0400

    cleanup template state example

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import ForgedMinionGame from './components/forged-minion'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <ForgedMinionGame />
   )
